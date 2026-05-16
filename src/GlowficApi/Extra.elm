09a43081de0c3e6f1b42d59619f125a3ba83766a
Leonardo Taglialegne <leonardo@taglialegne.it>
2026-05-17 00:08:01 +0200

    Working cache

--- src/GlowficApi/Extra.elm
+++ src/GlowficApi/Extra.elm
@@ -1,10 +1,17 @@
 module GlowficApi.Extra exposing (getPost, login)
 
 import BackendTask exposing (BackendTask)
+import BackendTask.Do as Do
 import BackendTask.Env as Env
+import BackendTask.File as File
+import BackendTask.Http as Http exposing (Body, Expect)
 import FatalError exposing (FatalError)
 import GlowficApi.Api
+import GlowficApi.Json
 import GlowficApi.Types exposing (PostDetails, Reply)
+import Json.Decode
+import Pages.Script as Script
+import Triple.Extra exposing (from)
 
 
 login : BackendTask FatalError { token : String }
@@ -26,19 +33,19 @@ login =
 
 
 getPost : { token : String } -> Int -> BackendTask FatalError ( PostDetails, List Reply )
-getPost { token } id =
-    GlowficApi.Api.getPostsId
-        { authorization = { authorization = token }
-        , params = { id = id }
+getPost authorization id =
+    getCachedWithAuthorization authorization
+        GlowficApi.Api.getPostsIdRecord
+        { params = { id = id }
         }
         |> BackendTask.andThen
             (\post ->
                 List.range 0 ((post.num_replies - 1) // 100)
                     |> List.map
                         (\page ->
-                            GlowficApi.Api.getPostsIdReplies
-                                { authorization = { authorization = token }
-                                , params =
+                            getCachedWithAuthorization authorization
+                                GlowficApi.Api.getPostsIdRepliesRecord
+                                { params =
                                     { id = id
                                     , page = Just page
                                     , per_page = Just 100
@@ -49,4 +56,40 @@ getPost { token } id =
                     |> BackendTask.map List.concat
                     |> BackendTask.map (\replies -> ( post, replies ))
             )
+
+
+getCachedWithAuthorization :
+    { token : String }
+    ->
+        ({ authorization : { authorization : String }, params : params }
+         ->
+            ( { url : String
+              , method : String
+              , headers : List ( String, String )
+              , body : Body
+              , retries : Maybe Int
+              , timeoutInMs : Maybe Int
+              }
+            , Expect result
+            )
+        )
+    -> { params : params }
+    -> BackendTask FatalError result
+getCachedWithAuthorization { token } toTuple { params } =
+    let
+        ( record, expect ) =
+            toTuple
+                { authorization = { authorization = token }
+                , params = params
+                }
+    in
+    Http.getWithOptions
+        { url = record.url
+        , expect = expect
+        , retries = record.retries
+        , timeoutInMs = record.timeoutInMs
+        , headers = record.headers
+        , cachePath = Just ".cache"
+        , cacheStrategy = Just Http.ForceCache
+        }
         |> BackendTask.allowFatal
